dc43cce1df724ba7694db99da5f1167d49abe0a4
Vladimir <martyshin.v@gmail.com>
2016-12-09 18:15:37 +0300

    Message

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'bootstrap-sass', '2.3.2.0'
 
 group :development, :test do 
 	# Use sqlite3 as the database for Active Record
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,45 +4,45 @@ describe "Static Pages" do
   describe "Home page" do
     it "should have the content 'Simple App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
-      #response.status.should be(200)
     end
     it "should have the title 'Home'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/home'
+      visit root_path
       expect(page).to have_title('Home')
-      #response.status.should be(200)
     end
   end
 
 describe "Help page" do
 	it "should have the content 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
-      #response.status.should be(200)
 	end
 	it "should have the title 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/help'
+      visit help_path
       expect(page).to have_title('Help')
-      #response.status.should be(200)
 	end
 end
 
 describe "About us" do
     it "should have the content 'About us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/about'
+      visit about_path
       expect(page).to have_content('About us')
-      #response.status.should be(200)
     end
     it "should have the title 'About us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/about'
+      visit about_path
       expect(page).to have_title('About us')
-      #response.status.should be(200)
+    end
+  end  
+
+describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title('Contact')
     end
   end  
 end
